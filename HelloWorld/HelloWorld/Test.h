aa7db3c1ff62dc9c5285dfa54c948254e1214957
lijinxuan <lijinxuanxinxi@126.com>
2013-07-15 17:41:57 +0800

    add file

---
+++ HelloWorld/HelloWorld/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  HelloWorld
+//
+//  Created by 李金轩 on 13-7-15.
+//  Copyright (c) 2013年 李金轩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ HelloWorld/HelloWorld/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  HelloWorld
+//
+//  Created by 李金轩 on 13-7-15.
+//  Copyright (c) 2013年 李金轩. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
